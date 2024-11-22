446d4968b0a40a99420ae3cfc9c303d8d2f6a4c3
Esoji <aim.wtf1234@gmail.com>
2024-11-22 17:07:53 +0300

    adding better verification of standing on the platform

--- Assets/Scripts/PlayerStatePatternExaple/Scripts/Player/GroundChecker.cs
+++ Assets/Scripts/PlayerStatePatternExaple/Scripts/Player/GroundChecker.cs
@@ -5,8 +5,20 @@ public class GroundChecker : MonoBehaviour
     [SerializeField] private LayerMask _ground;
 
     [SerializeField, Range(0.01f, 1)] private float _distanceToCheck;
+    [SerializeField] private Transform[] _transformCube;
 
     public bool IsTouches { get; private set; }
 
-    private void Update() => IsTouches = Physics.CheckSphere(transform.position, _distanceToCheck, _ground);
+    private void Update()
+    {
+        IsTouches = false;
+        foreach (Transform point in _transformCube)
+        {
+            if (Physics.CheckSphere(point.position, _distanceToCheck, _ground))
+            {
+                IsTouches = true; 
+                break; 
+            }
+        }
+    }
 }
